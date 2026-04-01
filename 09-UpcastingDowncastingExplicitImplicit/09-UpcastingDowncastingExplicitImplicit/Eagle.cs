3f2b65fe410f442b40a1015cacea5b2075c81a8a
EzetQelenderli <azatrg-apa202@code.edu.az>
2026-04-01 10:11:19 +0400

    task 9

---
+++ 09-UpcastingDowncastingExplicitImplicit/09-UpcastingDowncastingExplicitImplicit/Eagle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09_UpcastingDowncastingExplicitImplicit
+{
+    internal class Eagle : Animal
+    {
+        public int FlySpeed { get; set; }
+        public void Fly()
+        {
+            Console.WriteLine("flied away");
+        }
+    }
+}

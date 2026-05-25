8b367451d3478d13c2162040db2013be931fca9e
feyzullah1903 <feyzullaham-apa202@code.edu.az>
2026-05-25 12:28:01 +0400

    task

--- 27-FrontToBackSqlConnection/27-FrontToBackSqlConnection/obj/Debug/net10.0/27-FrontToBackSqlConnection.AssemblyInfo.cs
+++ 27-FrontToBackSqlConnection/27-FrontToBackSqlConnection/obj/Debug/net10.0/27-FrontToBackSqlConnection.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("27-FrontToBackSqlConnection")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8791efc847967dbea694615a16851db36134038")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b9e71515a40f5262e6543a27204a45d94665301")]
 [assembly: System.Reflection.AssemblyProductAttribute("27-FrontToBackSqlConnection")]
 [assembly: System.Reflection.AssemblyTitleAttribute("27-FrontToBackSqlConnection")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
